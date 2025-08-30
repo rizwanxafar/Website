05ab0cc3770bd0bc534ab097086129b9fba1c015
rizwanxafar <154759175+rizwanxafar@users.noreply.github.com>
2025-08-30 18:41:59 +0100

    Update page.js

--- src/app/tailwind-test/page.js
+++ src/app/tailwind-test/page.js
@@ -1,1 +1,7 @@
-
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500 text-white">
+      <h1 className="text-5xl font-bold">TAILWIND IS WORKING</h1>
+    </div>
+  );
+}
