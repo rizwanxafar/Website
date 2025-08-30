a75c7edb39093441112ab7b8445e6e15eded4e02
rizwanxafar <154759175+rizwanxafar@users.noreply.github.com>
2025-08-30 19:30:37 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 // src/app/layout.js
 import "./globals.css";
 import NavBar from "../components/NavBar";
-import { Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 
-const roboto = Roboto({
+const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
   display: "swap",
@@ -17,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={roboto.className}>
+      <body className={inter.className}>
         <NavBar />
         <main className="mx-auto w-full max-w-6xl px-3 sm:px-4 lg:px-6 py-4">
           {children}
