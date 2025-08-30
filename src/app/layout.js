fe44eb60ea377ec219f10ac3c9cb371a42991c7c
rizwanxafar <154759175+rizwanxafar@users.noreply.github.com>
2025-08-30 19:24:22 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body>
         <NavBar />
-        <main className="mx-auto w-full max-w-5xl px-4 sm:px-6 lg:px-8 py-6">
+        <main className="mx-auto w-full max-w-6xl px-3 sm:px-4 lg:px-6 py-4">
           {children}
         </main>
       </body>
