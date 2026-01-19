81e381a21d23cfc5c6a8a346afd8f3de74810750
rizwanxafar <154759175+rizwanxafar@users.noreply.github.com>
2026-01-19 12:29:14 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: "media",
   content: [
     "./src/**/*.{js,jsx,ts,tsx,mdx}",
     "./src/app/**/*.{js,jsx,ts,tsx,mdx}",
     "./src/components/**/*.{js,jsx,ts,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        // We map these to the CSS variables defined in globals.css
+        brand: "hsl(var(--brand) / <alpha-value>)",
+        brandAlt: "hsl(var(--brand-alt) / <alpha-value>)",
+        accent: "hsl(var(--accent) / <alpha-value>)",
+        midnight: "hsl(var(--midnight) / <alpha-value>)",
+      }
+    },
+  },
   plugins: [],
 };
